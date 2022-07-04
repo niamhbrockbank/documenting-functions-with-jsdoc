fab5e9538df8968892b7cd99cdd572a8fe7195fa
Niamh <niamh.brockbank@gmail.com>
2022-07-04 15:55:31 +0100

    Added documentation for asNegative

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,18 @@
 // TODO (Ex.0): document and write tests for this function
-
+/**
+ * Returns the negative of the given number
+ * @param {number} n 
+ * @returns {number} the negative of n
+ */
 function asNegative(n) {
-  // ...
+  if (n > 0){
+    return -n
+  } else {
+    return n
+  }
 }
 
+console.log(asNegative(5),-5)
+console.log(asNegative(-6), -6)
+console.log(asNegative(0.5), -0.5)
 
